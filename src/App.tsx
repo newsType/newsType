a7065f6c7371b6138bcb427c29d825d7497c55a4
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-01-09 15:18:34 +0900

    typescript 뉴스 뼈대 완성

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Detail from './pages/Detail';
+import HomePage from './pages/HomePage';
+import './styles/common.scss';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
