d6fec68fc67974934662992d23eec946a30ce35a
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-01-11 17:26:05 +0900

    메인에 올리기 1트

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import Detail from './pages/Detail';
-import HomePage from './pages/HomePage';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/detail" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
