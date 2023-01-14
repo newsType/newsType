07bb95f5246da74e3526891185ed19863953b28a
rondido <rppr001@naver.com>
2023-01-14 17:12:09 +0900

    feat: Head google font commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router';
 import Footer from './components/common/Footer';
 import Detail from './Detail';
+import Header from './components/common/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Detail />
       <BrowserRouter>
         <Routes></Routes>
