d7c4e84f3cdf71a22f90b6ff9f41aae52026c042
rondido <rppr001@naver.com>
2023-01-17 16:55:01 +0900

    feat: rtk query setting and api get commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router';
 import Footer from './components/common/Footer';
-import Detail from './Detail';
 import Header from './components/common/Header';
-
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Detail />
-      <BrowserRouter>
-        <Routes></Routes>
-      </BrowserRouter>
+      <Main/>
       <Footer/>
-
     </div>
   );
 }
